11dd30c25638f6f1d255298609f823c4ea4c4027
Adam Smith <adam.george.smith@btinternet.com>
2017-12-06 10:07:04 +0000

    Ocl Module Header

---
+++ include/anvil/Ocl.hpp
@@ -0,0 +1,28 @@
+//Copyright 2017 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#ifndef ANVIL_OCL_HPP
+#define ANVIL_OCL_HPP
+
+#include "anvil/ocl/Core.hpp"
+#include "anvil/ocl/Device.hpp"
+#include "anvil/ocl/Platform.hpp"
+#include "anvil/ocl/Context.hpp"
+#include "anvil/ocl/Event.hpp"
+#include "anvil/ocl/CommandQueue.hpp"
+#include "anvil/ocl/Buffer.hpp"
+#include "anvil/ocl/Program.hpp"
+#include "anvil/ocl/Kernel.hpp"
+
+#endif
