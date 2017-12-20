3a9dc4f49516c274052176e86e44cc0b051900c3
Adam Smith <adam.george.smith@btinternet.com>
2017-12-20 10:37:11 +0000

    De-designed PrimativeValue
    Renamed AnyPrimative.hpp to Value.hpp

---
+++ include/anvil/maths/Value.hpp
@@ -0,0 +1,144 @@
+//Copyright 2017 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#ifndef ANVIL_MATHS_ANY_PRIMATIVE_HPP
+#define ANVIL_MATHS_ANY_PRIMATIVE_HPP
+
+#include "anvil/core/Keywords.hpp"
+#include "anvil/maths/Type.hpp"
+
+namespace anvil {
+
+
+	class Value {
+	public:
+		enum {
+			MAX_LENGTH = 4
+		};
+		union {
+			uint8_t u8[MAX_LENGTH];		//!< Unsigned 8 bit value.
+			uint16_t u16[MAX_LENGTH];	//!< Unsigned 16 bit value.
+			uint32_t u32[MAX_LENGTH];	//!< Unsigned 32 bit value.
+			uint64_t u64[MAX_LENGTH];	//!< Unsigned 64 bit value.
+			int8_t s8[MAX_LENGTH];		//!< Signed 8 bit value.
+			int16_t s16[MAX_LENGTH];	//!< Signed 16 bit value.
+			int32_t s32[MAX_LENGTH];	//!< Signed 32 bit value.
+			int64_t s64[MAX_LENGTH];	//!< Signed 64 bit value.
+			float32_t f32[MAX_LENGTH];	//!< Single precision floating point value.
+			float64_t f64[MAX_LENGTH];	//!< Double precision floating point value.
+			bool b8[MAX_LENGTH];		//!< Boolean value.x
+		};
+		Type type;
+		uint8_t length;
+
+		ANVIL_CALL Value() throw();
+
+#define ANVIL_DEF_FUNCTIONS(type)\
+		ANVIL_CALL Value(type) throw();\
+		ANVIL_CALL Value(const type *, size_t) throw();\
+		ANVIL_CALL operator type() const throw();\
+		ANVIL_CALL operator const type *() const throw();\
+
+		ANVIL_DEF_FUNCTIONS(uint8_t)
+		ANVIL_DEF_FUNCTIONS(int8_t)
+		ANVIL_DEF_FUNCTIONS(uint16_t)
+		ANVIL_DEF_FUNCTIONS(int16_t)
+		ANVIL_DEF_FUNCTIONS(uint32_t)
+		ANVIL_DEF_FUNCTIONS(int32_t)
+		ANVIL_DEF_FUNCTIONS(uint64_t)
+		ANVIL_DEF_FUNCTIONS(int64_t)
+		ANVIL_DEF_FUNCTIONS(float32_t)
+		ANVIL_DEF_FUNCTIONS(float64_t)
+		ANVIL_DEF_FUNCTIONS(bool)
+#undef ANVIL_DEF_FUNCTIONS
+	};
+
+	/*!
+		\brief Dispatch a function call based on a primative type.
+		\tparam FU8 Functor type if aType is ANVIL_8U.
+		\tparam FU16 Functor type if aType is ANVIL_16U.
+		\tparam FU32 Functor type if aType is ANVIL_32U.
+		\tparam FU64 Functor type if aType is ANVIL_64U.
+		\tparam FS8 Functor type if aType is ANVIL_8S.
+		\tparam FS16 Functor type if aType is ANVIL_16S.
+		\tparam FS32 Functor type if aType is ANVIL_32S.
+		\tparam FS64 Functor type if aType is ANVIL_64S.
+		\tparam FF32 Functor type if aType is ANVIL_32F.
+		\tparam FF64 Functor type if aType is ANVIL_64F.
+		\tparam FB8 Functor type if aType is ANVIL_8B.
+		\param aPrimative The container for the primative value.
+		\param aType The type contained in aType.
+		\param fu8 Functor to call if aType is ANVIL_8U.
+		\param fu16 Functor to call if aType is ANVIL_16U.
+		\param fu32 Functor to call if aType is ANVIL_32U.
+		\param fu64 Functor to call if aType is ANVIL_64U.
+		\param fs8 Functor to call if aType is ANVIL_8S.
+		\param fs16 Functor to call if aType is ANVIL_16S.
+		\param fs32 Functor to call if aType is ANVIL_32S.
+		\param fs64 Functor to call if aType is ANVIL_64S.
+		\param ff32 Functor to call if aType is ANVIL_32F.
+		\param ff64 Functor to call if aType is ANVIL_64F.
+		\param fb8 Functor to call if aType is ANVIL_8B.
+		\return True if one of the functors was called, otherwise false (aType is not a primative type).
+	*/
+	template<class FU8, class FU16, class FU32, class FU64, 
+		class FS8, class FS16, class FS32, class FS64, 
+		class FF32, class FF64, class FB8>
+	static inline bool ANVIL_CALL dispatchByType(Value aValue, 
+	FU8 fu8,  FU16 fu16,  FU32 fu32,  FU64 fu64,  
+	FS8 fs8,  FS16 fs16,  FS32 fs32,  FS64 fs64, 
+	FF32 ff32,  FF64 ff64, FB8 fb8) {
+		bool returnValue = true;
+		switch(GetPrimativeType(aPrimative.type)) {
+		case ANVIL_8U:
+			fu8(aValue.u8, aValue.length);
+			break;
+		case ANVIL_8S:
+			fs8(aValue.s8, aValue.length);
+			break;
+		case ANVIL_16U:
+			fu16(aValue.u16, aValue.length);
+			break;
+		case ANVIL_16S:
+			fs16(aValue.s16, aValue.length);
+			break;
+		case ANVIL_32U:
+			fu32(aValue.u32, aValue.length);
+			break;
+		case ANVIL_32S:
+			fs32(aValue.s32, aValue.length);
+			break;
+		case ANVIL_64U:
+			fu64(aValue.u64, aValue.length);
+			break;
+		case ANVIL_64S:
+			fs64(aValue.s64, aValue.length);
+			break;
+		case ANVIL_32F:
+			ff32(aValue.f32, aValue.length);
+			break;
+		case ANVIL_64F:
+			ff64(aValue.f64, aValue.length);
+		case ANVIL_8B:
+			fb8(aValue.b8, aValue.length);
+			break;
+		default:
+			returnValue = false;
+			break;
+		}
+		return returnValue;
+	}
+}
+
+#endif
---
+++ src/anvil/maths/Value.cpp
@@ -0,0 +1,127 @@
+//Copyright 2017 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include "anvil/maths/Value.hpp"
+
+namespace anvil {
+
+	// Value
+
+	ANVIL_CALL Value::Value() throw() :
+		type(ANVIL_8U),
+		length(0)
+	{
+		for (int i = 0; i < MAX_LENGTH; ++i) u8[i] = 0;
+	}
+
+#define ANVIL_DEF_FUNCTIONS(E, T, N)\
+	ANVIL_CALL Value::Value(T aValue) throw() :\
+		type(E),\
+		length(1)\
+					{\
+		N[0] = aValue;\
+		for (int i = 1; i < MAX_LENGTH; ++i) N[i] = static_cast<T>(0);\
+					}\
+	ANVIL_CALL Value::Value(const T* aValue, size_t aLength) throw() :\
+		type(E),\
+		length(aLength < MAX_LENGTH ? aLength : MAX_LENGTH)\
+					{\
+			const int l = length;\
+			for (int i = 0; i < l; ++i) N[i] = aValue[i];\
+					}\
+	ANVIL_CALL Value::operator T() const throw() {\
+		switch(GetPrimativeType(type)) {\
+		case ANVIL_8U:\
+			return static_cast<T>(u8[0]);\
+		case ANVIL_8S:\
+			return static_cast<T>(s8[0]);\
+		case ANVIL_16U:\
+			return static_cast<T>(u16[0]);\
+		case ANVIL_16S:\
+			return static_cast<T>(s16[0]);\
+		case ANVIL_32U:\
+			return static_cast<T>(u32[0]);\
+		case ANVIL_32S:\
+			return static_cast<T>(s32[0]);\
+		case ANVIL_64U:\
+			return static_cast<T>(u64[0]);\
+		case ANVIL_64S:\
+			return static_cast<T>(s64[0]);\
+		case ANVIL_32F:\
+			return static_cast<T>(f32[0]);\
+		case ANVIL_64F:\
+			return static_cast<T>(f64[0]);\
+		case ANVIL_8B:\
+			return static_cast<T>(b8[0]);\
+		default:\
+			return static_cast<T>(0);\
+		}\
+		}\
+	ANVIL_CALL Value::operator const T*() const throw() {\
+		static T gBuffer[MAX_LENGTH];\
+		const int l = length;\
+		switch(GetPrimativeType(type)) {\
+		case ANVIL_8U:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(u8[i]);\
+			break;\
+		case ANVIL_8S:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(s8[i]);\
+			break;\
+		case ANVIL_16U:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(u16[i]);\
+			break;\
+		case ANVIL_16S:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(s16[i]);\
+			break;\
+		case ANVIL_32U:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(u32[i]);\
+			break;\
+		case ANVIL_32S:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(s32[i]);\
+			break;\
+		case ANVIL_64U:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(u64[i]);\
+			break;\
+		case ANVIL_64S:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(s64[i]);\
+			break;\
+		case ANVIL_32F:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(f32[i]);\
+			break;\
+		case ANVIL_64F:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(f64[i]);\
+			break;\
+		case ANVIL_8B:\
+			for(int i = 0; i < l; ++i) gBuffer[i] = static_cast<T>(b8[i]);\
+			break;\
+		default:\
+			break;\
+		}\
+		return gBuffer;\
+	}
+
+	ANVIL_DEF_FUNCTIONS(ANVIL_8U, uint8_t, u8)
+	ANVIL_DEF_FUNCTIONS(ANVIL_8S, int8_t, s8)
+	ANVIL_DEF_FUNCTIONS(ANVIL_16U, uint16_t, u16)
+	ANVIL_DEF_FUNCTIONS(ANVIL_16S, int16_t, s16)
+	ANVIL_DEF_FUNCTIONS(ANVIL_32U, uint32_t, u32)
+	ANVIL_DEF_FUNCTIONS(ANVIL_32S, int32_t, s32)
+	ANVIL_DEF_FUNCTIONS(ANVIL_64U, uint64_t, u64)
+	ANVIL_DEF_FUNCTIONS(ANVIL_64S, int64_t, s64)
+	ANVIL_DEF_FUNCTIONS(ANVIL_32F, float32_t, f32)
+	ANVIL_DEF_FUNCTIONS(ANVIL_64F, float64_t, f64)
+	ANVIL_DEF_FUNCTIONS(ANVIL_8B, bool, b8)
+#undef ANVIL_DEF_FUNCTIONS
+
+}
