1519271a718550a174984a5b0dfb1bab0a4b3153
Adam Smith <adam.george.smith@btinternet.com>
2018-01-19 09:52:30 +0000

    Additional Vectorise Functions

--- include/anvil/maths/Vectorise.hpp
+++ include/anvil/maths/Vectorise.hpp
@@ -23,19 +23,15 @@ namespace anvil {
 	template<class T>\
 	static void NAME(T* ANVIL_RESTRICT a, const T b, size_t a_length) throw() {\
 		enum { LENGTH = detail::OptimalVectorLength<T, VOP>::value };\
-		\
 		detail::VectorPtr<T, LENGTH> a_;\
-		\
 		a_.scalar = a;\
-		\
 		Vector<T,LENGTH> scalar;\
 		for(int i = 0; i < LENGTH; ++i) scalar[i] = b;\
 		if (LENGTH > 1) while (a_length >= LENGTH) {\
 			OPERATION(*a_.vector, scalar);\
 			++a_.vector;\
 			a_length -= LENGTH;\
-				}\
-		\
+		}\
 		for (size_t i = 0; i < a_length; ++i) {\
 			OPERATION(*a_.scalar, b);\
 			++a_.scalar;\
@@ -47,20 +43,16 @@ namespace anvil {
 	template<class T>\
 	static void NAME(T* ANVIL_RESTRICT a, const T* ANVIL_RESTRICT b, size_t a_length) throw() {\
 		enum { LENGTH = detail::OptimalVectorLength<T, VOP>::value };\
-		\
 		detail::VectorPtr<T, LENGTH> a_;\
 		detail::VectorPtr<T, LENGTH> b_;\
-		\
 		a_.scalar = a;\
 		b_.scalar = const_cast<T*>(b);\
-		\
 		if (LENGTH > 1) while (a_length >= LENGTH) {\
 			OPERATION(*a_.vector, *b_.vector);\
 			++a_.vector;\
 			++b_.vector;\
 			a_length -= LENGTH;\
 		}\
-		\
 		for (size_t i = 0; i < a_length; ++i) {\
 			OPERATION(*a_.scalar, *b_.scalar);\
 			++a_.scalar;\
@@ -72,18 +64,14 @@ namespace anvil {
 	template<class T>\
 	static void NAME(const T* ANVIL_RESTRICT a, const T b, T* ANVIL_RESTRICT c, size_t a_length) throw() {\
 		enum { LENGTH = detail::OptimalVectorLength<T, VOP>::value };\
-		\
 		if (a == c) {\
 			NAME<T>(c, b, a_length);\
 			return;\
 		}\
-		\
 		detail::VectorPtr<T, LENGTH> a_;\
 		detail::VectorPtr<T, LENGTH> c_;\
-		\
 		a_.scalar = const_cast<T*>(a);\
 		c_.scalar = c;\
-		\
 		Vector<T,LENGTH> scalar;\
 		for(int i = 0; i < LENGTH; ++i) scalar[i] = b;\
 		if (LENGTH > 1) while (a_length >= LENGTH) {\
@@ -92,7 +80,6 @@ namespace anvil {
 			++c_.vector;\
 			a_length -= LENGTH;\
 		}\
-		\
 		for (size_t i = 0; i < a_length; ++i) {\
 			*c_.scalar = OPERATION(*a_.scalar, b);\
 			++a_.scalar;\
@@ -106,20 +93,16 @@ namespace anvil {
 	template<class T>\
 	static void NAME(const T* ANVIL_RESTRICT a, const T* ANVIL_RESTRICT b, T* ANVIL_RESTRICT c, size_t a_length) throw() {\
 		enum { LENGTH = detail::OptimalVectorLength<T, VOP>::value };\
-		\
 		if (a == c) {\
 			NAME<T>(c, b, a_length);\
 			return;\
 		}\
-		\
 		detail::VectorPtr<T, LENGTH> a_;\
 		detail::VectorPtr<T, LENGTH> b_;\
 		detail::VectorPtr<T, LENGTH> c_;\
-		\
 		a_.scalar = const_cast<T*>(a);\
 		b_.scalar = const_cast<T*>(b);\
 		c_.scalar = c;\
-		\
 		if (LENGTH > 1) while (a_length >= LENGTH) {\
 			*c_.vector = OPERATION(*a_.vector, *b_.vector);\
 			++a_.vector;\
@@ -127,7 +110,6 @@ namespace anvil {
 			++c_.vector;\
 			a_length -= LENGTH;\
 		}\
-		\
 		for (size_t i = 0; i < a_length; ++i) {\
 			*c_.scalar = OPERATION(*a_.scalar, *b_.scalar);\
 			++a_.scalar;\
@@ -177,6 +159,42 @@ ANVIL_VECTORISE_VVV(detail::VOP_OR, vector_or, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2
 #define ANVIL_VECTOR_OP(X,Y) X ^ Y
 #define ANVIL_VECTOR_OP2(X, Y) X ^= Y
 ANVIL_VECTORISE_VVV(detail::VOP_XOR, vector_xor, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
+
+#undef ANVIL_VECTOR_OP
+#undef ANVIL_VECTOR_OP2
+#define ANVIL_VECTOR_OP(X,Y) X == Y
+#define ANVIL_VECTOR_OP2(X, Y) X = X == Y
+ANVIL_VECTORISE_VVV(detail::VOP_EQ, vector_eq, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
+
+#undef ANVIL_VECTOR_OP
+#undef ANVIL_VECTOR_OP2
+#define ANVIL_VECTOR_OP(X,Y) X != Y
+#define ANVIL_VECTOR_OP2(X, Y) X = X != Y
+ANVIL_VECTORISE_VVV(detail::VOP_NE, vector_ne, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
+
+#undef ANVIL_VECTOR_OP
+#undef ANVIL_VECTOR_OP2
+#define ANVIL_VECTOR_OP(X,Y) X < Y
+#define ANVIL_VECTOR_OP2(X, Y) X = X < Y
+ANVIL_VECTORISE_VVV(detail::VOP_LT, vector_lt, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
+
+#undef ANVIL_VECTOR_OP
+#undef ANVIL_VECTOR_OP2
+#define ANVIL_VECTOR_OP(X,Y) X > Y
+#define ANVIL_VECTOR_OP2(X, Y) X = X > Y
+ANVIL_VECTORISE_VVV(detail::VOP_GT, vector_gt, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
+
+#undef ANVIL_VECTOR_OP
+#undef ANVIL_VECTOR_OP2
+#define ANVIL_VECTOR_OP(X,Y) X <= Y
+#define ANVIL_VECTOR_OP2(X, Y) X = X <= Y
+ANVIL_VECTORISE_VVV(detail::VOP_LE, vector_le, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
+
+#undef ANVIL_VECTOR_OP
+#undef ANVIL_VECTOR_OP2
+#define ANVIL_VECTOR_OP(X,Y) X >= Y
+#define ANVIL_VECTOR_OP2(X, Y) X = X >= Y
+ANVIL_VECTORISE_VVV(detail::VOP_GE, vector_ge, ANVIL_VECTOR_OP, ANVIL_VECTOR_OP2)
 
 }
 
