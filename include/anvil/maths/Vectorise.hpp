e4f28b4c0778c50e7fec1354856161f8ab5ae972
Adam Smith <adam.george.smith@btinternet.com>
2018-01-19 12:41:41 +0000

    Vectorise Multi-Channel Scalars

--- include/anvil/maths/Vectorise.hpp
+++ include/anvil/maths/Vectorise.hpp
@@ -153,6 +153,71 @@ namespace anvil {
 			}
 		};
 
+		template<class T, size_t VEC_LEN, VectorOp VOP>
+		static void vector_vsv_(const T* a, const T* b, T* c, size_t a_size) throw() {
+			typedef Vector<T, VEC_LEN> Vec;
+			typedef Vector<T, 1> Sca;
+			typedef VopDispatch<T, VEC_LEN, VOP> VopVec;
+			typedef VopDispatch<T, 1, VOP> VopSca;
+
+			const size_t loop = a_size / VEC_LEN;
+			const size_t remainder = a_size % VEC_LEN;
+			if (remainder != 0) return; //! \todo Handle trailing elements
+
+			const Vec* const aVec = reinterpret_cast<const Vec*>(a);
+			const Vec bVec = *reinterpret_cast<const Vec*>(b);
+			Vec* const cVec = reinterpret_cast<Vec*>(c);
+
+			if (c == a) {
+				for (size_t i = 0; i < loop; ++i) VopVec::call_assignment(cVec[i], bVec);
+				//for (size_t i = 0; i < remainder; ++i) VopSca::call_assignment(cSca[i], bSca);
+			} else {
+				for (size_t i = 0; i < loop; ++i) cVec[i] = VopVec::call(aVec[i], bVec);
+				//for (size_t i = 0; i < remainder; ++i) cSca[i] = VopSca::call(aSca[i], bSca);
+			}
+#undef ANVIL_PAD_VECTOR
+		}
+
+		template<class T, size_t VEC_LEN, VectorOp VOP>
+		static void vector_vsv(const T* a, const T* b, T* c, size_t a_size) throw() {
+			if (VEC_LEN == 1) {
+				vector_vsv<T, VOP>(a, *b, c, a_size);
+			} else {
+				enum {
+					LEN_2 = VEC_LEN * 2,
+					LEN_4 = VEC_LEN * 4,
+					LEN_8 = VEC_LEN * 8,
+					BYTES = sizeof(T) * VEC_LEN
+				};
+				if (VopOptimised<T, LEN_8, VOP>::value && (a_size % 8) == 0) {
+					T buffer[LEN_8];
+					memcpy(buffer, b, BYTES);
+					memcpy(buffer + VEC_LEN, b, BYTES);
+					memcpy(buffer + (VEC_LEN * 2), b, BYTES);
+					memcpy(buffer + (VEC_LEN * 3), b, BYTES);
+					memcpy(buffer + (VEC_LEN * 4), b, BYTES);
+					memcpy(buffer + (VEC_LEN * 5), b, BYTES);
+					memcpy(buffer + (VEC_LEN * 6), b, BYTES);
+					memcpy(buffer + (VEC_LEN * 7), b, BYTES);
+					vector_vsv_<T, LEN_8, VOP>(a, buffer, c, a_size);
+				} else if (VopOptimised<T, LEN_4, VOP>::value && (a_size % 4) == 0) {
+					T buffer[LEN_4];
+					memcpy(buffer, b, BYTES);
+					memcpy(buffer + VEC_LEN, b, BYTES);
+					memcpy(buffer + (VEC_LEN * 2), b, BYTES);
+					memcpy(buffer + (VEC_LEN * 3), b, BYTES);
+					vector_vsv_<T, LEN_4, VOP>(a, buffer, c, a_size);
+				} else if (VopOptimised<T, LEN_2, VOP>::value && (a_size % 2) == 0) {
+					T buffer[LEN_2];
+					memcpy(buffer, b, BYTES);
+					memcpy(buffer + VEC_LEN, b, BYTES);
+					vector_vsv_<T, LEN_2, VOP>(a, buffer, c, a_size);
+				} else {
+					vector_vsv_<T, VEC_LEN, VOP>(a, b, c, a_size);
+				}
+			}
+		}
+
 		template<class T, VectorOp VOP>
 		static void vector_vsv(const T* a, T b, T* c, size_t a_size) throw() {
 			enum { LENGTH = detail::OptimalVectorLength<T, VOP>::value };
@@ -218,6 +283,10 @@ static inline void NAME(const T* a, const T* b, T* c, size_t a_size) throw() {\
 template<class T>\
 static inline void NAME(const T* a, const T b, T* c, size_t a_size) throw() {\
 	detail::vector_vsv<T, VOP>(a, b, c, a_size);\
+}\
+template<class T, size_t VEC_LEN>\
+static inline void NAME ## _scalar(const T* a, const T* b, T* c, size_t a_size) throw() {\
+	detail::vector_vsv<T, VEC_LEN, VOP>(a, b, c, a_size);\
 }\
 
 ANVIL_VECTORISE_VVV(detail::VOP_ADD, vector_add)
