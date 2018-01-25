3b9696771baa5c3c873552ee18252b47dc57e398
Adam Smith <adam.george.smith@btinternet.com>
2018-01-25 21:42:06 +0000

    Improved Vectoriseation

--- include/anvil/maths/Vectorise.hpp
+++ include/anvil/maths/Vectorise.hpp
@@ -19,96 +19,85 @@
 
 namespace anvil {
 	namespace simd {
+
 		template<class T, Operation O>
-		static void ANVIL_CALL dynamic_operation(const T* x, const T* y, const T* z, T* o, const size_t s) {
+		static void ANVIL_CALL dynamic_operation(const T* x_, const T* y_, const T* z_, T* o_, const size_t s) {
 			enum { OPTIMAL = OptimalOperationSize<O, T>::value };
 			typedef OperationImplementation<T, OPTIMAL, O> optimal_t;
+			typedef DefaultSIMD<T, OPTIMAL> optimal_simd_t;
+
+			const optimal_simd_t* const x = reinterpret_cast<const optimal_simd_t*>(x_);
+			const optimal_simd_t* const y = reinterpret_cast<const optimal_simd_t*>(y_);
+			const optimal_simd_t* const z = reinterpret_cast<const optimal_simd_t*>(z_);
+			optimal_simd_t* const o = reinterpret_cast<optimal_simd_t*>(o_);
 
 			const size_t loop = s / OPTIMAL;
 			const size_t remainder = s % OPTIMAL;
 
 			if (optimal_t::optimised()) {
 				for (size_t i = 0; i < loop; ++i) {
-					optimal_t::execute_op(x, y, z, o);
-					x += OPTIMAL;
-					y += OPTIMAL;
-					z += OPTIMAL;
-					o += OPTIMAL;
+					optimal_t::execute_op(x[i].elements, y[i].elements, z[i].elements, o[i].elements);
 				}
 			} else {
 				for (size_t i = 0; i < loop; ++i) {
-					optimal_t::execute_nop(x, y, z, o);
-					x += OPTIMAL;
-					y += OPTIMAL;
-					z += OPTIMAL;
-					o += OPTIMAL;
+					optimal_t::execute_nop(x[i].elements, y[i].elements, z[i].elements, o[i].elements);
 				}
 			}
 			for (size_t i = 0; i < remainder; ++i) {
-				o[i] = OperationImplementation<T, 1, O>::execute(*x, *y, *z);
-				++x;
-				++y;
-				++z;
-				++o;
+				o[loop].elements[i] = OperationImplementation<T, 1, O>::execute(x[loop].elements[i], y[loop].elements[i], z[loop].elements[i]);
 			}
 		}
 
 		template<class T, Operation O>
-		static void ANVIL_CALL dynamic_operation(const T* x, const T* y, T* o, const size_t s) {
+		static void ANVIL_CALL dynamic_operation(const T* x_, const T* y_, T* o_, const size_t s) {
 			enum { OPTIMAL = OptimalOperationSize<O, T>::value };
 			typedef OperationImplementation<T, OPTIMAL, O> optimal_t;
+			typedef DefaultSIMD<T, OPTIMAL> optimal_simd_t;
+
+			const optimal_simd_t* const x = reinterpret_cast<const optimal_simd_t*>(x_);
+			const optimal_simd_t* const y = reinterpret_cast<const optimal_simd_t*>(y_);
+			optimal_simd_t* const o = reinterpret_cast<optimal_simd_t*>(o_);
 
 			const size_t loop = s / OPTIMAL;
 			const size_t remainder = s % OPTIMAL;
 
 			if (optimal_t::optimised()) {
 				for (size_t i = 0; i < loop; ++i) {
-					optimal_t::execute_op(x, y, o);
-					x += OPTIMAL;
-					y += OPTIMAL;
-					o += OPTIMAL;
+					optimal_t::execute_op(x[i].elements, y[i].elements, o[i].elements);
 				}
 			} else {
 				for (size_t i = 0; i < loop; ++i) {
-					optimal_t::execute_nop(x, y, o);
-					x += OPTIMAL;
-					y += OPTIMAL;
-					o += OPTIMAL;
+					optimal_t::execute_nop(x[i].elements, y[i].elements, o[i].elements);
 				}
 			}
 			for (size_t i = 0; i < remainder; ++i) {
-				o[i] = OperationImplementation<T, 1, O>::execute(*x, *y);
-				++x;
-				++y;
-				++o;
+				o[loop].elements[i] = OperationImplementation<T, 1, O>::execute(x[loop].elements[i], y[loop].elements[i]);
 			}
 		}
 
 		template<class T, Operation O>
-		static void ANVIL_CALL dynamic_operation(const T* x, T* o, const size_t s) {
+		static void ANVIL_CALL dynamic_operation(const T* x_, T* o_, const size_t s) {
 			enum { OPTIMAL = OptimalOperationSize<O, T>::value };
 			typedef OperationImplementation<T, OPTIMAL, O> optimal_t;
+			typedef DefaultSIMD<T, OPTIMAL> optimal_simd_t;
+
+			const optimal_simd_t* const x = reinterpret_cast<const optimal_simd_t*>(x_);
+			optimal_simd_t* const o = reinterpret_cast<optimal_simd_t*>(o_);
 
 			const size_t loop = s / OPTIMAL;
 			const size_t remainder = s % OPTIMAL;
 
 			if (optimal_t::optimised()) {
 				for (size_t i = 0; i < loop; ++i) {
-					optimal_t::execute_op(x, o);
-					x += OPTIMAL;
-					o += OPTIMAL;
+					optimal_t::execute_op(x[i].elements, o[i].elements);
 				}
 			} else {
 				for (size_t i = 0; i < loop; ++i) {
-					optimal_t::execute_nop(x, o);
-					x += OPTIMAL;
-					o += OPTIMAL;
+					optimal_t::execute_nop(x[i].elements, o[i].elements);
 				}
 			}
 			for (size_t i = 0; i < remainder; ++i) {
-				o[i] = OperationImplementation<T, 1, O>::execute(*x);
-				++x;
-				++o;
+				o[loop].elements[i] = OperationImplementation<T, 1, O>::execute(x[loop].elements[i]);
 			}
 		}
 	}
