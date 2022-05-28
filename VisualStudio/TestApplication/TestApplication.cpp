b9624fe0582dec5a346749368a8a4974603dfbe9
Adam Smith <adam.george.smith@btinternet.com>
2022-05-28 13:36:00 +0100

    Test code

--- VisualStudio/TestApplication/TestApplication.cpp
+++ VisualStudio/TestApplication/TestApplication.cpp
@@ -1,65 +1,138 @@
 #include <iostream>
 #include <bitset>
+#include <fstream>
 #include "anvil/Core.hpp"
 #include "anvil/Compute.hpp"
+#include "anvil/BytePipe.hpp"
+#include "anvil/byte-pipe/BytePipeJSON.hpp"
 
 int main()
 {
+	//{
+	//	anvil::BytePipe::JsonWriter out_pipe;
+
+	//	std::map<std::string, std::vector<int>> map_test;
+
+	//	{
+	//		std::vector<int>& tmp = map_test.emplace("even", std::vector<int>()).first->second;
+	//		for (int i = 0; i <= 10; ++i) if ((i & 1) == 0) tmp.push_back(i);
+	//	}
+
+	//	{
+	//		std::vector<int>& tmp = map_test.emplace("odd", std::vector<int>()).first->second;
+	//		for (int i = 0; i <= 10; ++i) if ((i & 1) != 0) tmp.push_back(i);
+	//	}
+
+	//	out_pipe(map_test);
+
+	//	std::cout << out_pipe.GetJSON() << std::endl;
+	//}
 	{
-		typedef anvil::Vector<float, 192> Vector;
+		std::map<std::string, std::vector<int>> map_test;
+		std::map<std::string, std::vector<int>> in_test;
 
-		Vector a, b, c;
-		anvil::Bitfield256 mask;
+		{
+			std::ofstream ofile("D:\\Adam\\Documents\\GitHub\\anvil\\test.bin", std::ios::binary);
+			anvil::BytePipe::OStreamPipe out_pipe(ofile);
+			anvil::BytePipe::Writer writer(out_pipe);
 
-		a = anvil::VectorAdd(b, c, a, mask);
 
+			{
+				std::vector<int>& tmp = map_test.emplace("even", std::vector<int>()).first->second;
+				for (int i = 0; i <= 10; ++i) if ((i & 1) == 0) tmp.push_back(i);
+			}
+
+			{
+				std::vector<int>& tmp = map_test.emplace("odd", std::vector<int>()).first->second;
+				for (int i = 0; i <= 10; ++i) if ((i & 1) != 0) tmp.push_back(i);
+			}
+
+			writer.OnPipeOpen();
+			writer(map_test);
+			writer.OnPipeClose();
+		}
+		{
+			std::ifstream ifile("D:\\Adam\\Documents\\GitHub\\anvil\\test.bin", std::ios::binary);
+			anvil::BytePipe::IStreamPipe in_pipe(ifile);
+			anvil::BytePipe::ValueParser parser;
+			anvil::BytePipe::Reader reader(in_pipe);
+			reader.Read(parser);
+
+			in_test = static_cast<decltype(in_test)>(parser.GetValue());
+		}
 	}
+
 	system("pause");
 
+	return 0;
 
-	enum { size = 16 };
-	anvil::Vector<float, size> a;
-	anvil::Vector<float, size> b;
-	anvil::Vector<float, size> c;
+	//{
+	//	enum { size = 192 };
+	//	typedef anvil::Vector<float, size> Vector;
 
-	//std::cin >> a[0u];
-	//std::cin >> b[0u];
-	//std::cin >> c[0u];
+	//	Vector a, b, c;
+	//	anvil::Bitfield256 mask;
 
-	//for (size_t i = 1u; i < size; ++i) {
-	//	a[i] = a[0u];
-	//	b[i] = b[0u];
-	//	c[i] = c[0u];
+
+	//	for (size_t i = 0u; i < size; ++i) {
+	//		a[i] = rand();
+	//		b[i] = rand();
+	//		c[i] = rand();
+	//	}
+	//	mask.low.low = rand();
+	//	mask.low.high = rand();
+	//	mask.high.low = rand();
+	//	mask.high.high = rand();
+
+	//	a = anvil::VectorAdd(b, c, a, mask);
+
+	//}
+	//system("pause");
+
+
+	//enum { size = 16 };
+	//anvil::Vector<float, size> a;
+	//anvil::Vector<float, size> b;
+	//anvil::Vector<float, size> c;
+
+	////std::cin >> a[0u];
+	////std::cin >> b[0u];
+	////std::cin >> c[0u];
+
+	////for (size_t i = 1u; i < size; ++i) {
+	////	a[i] = a[0u];
+	////	b[i] = b[0u];
+	////	c[i] = c[0u];
+	////}
+
+	//for (size_t i = 0u; i < size; ++i) {
+	//	a[i] = rand();
+	//	b[i] = rand();
+	//	c[i] = rand();
 	//}
 
-	for (size_t i = 0u; i < size; ++i) {
-		a[i] = rand();
-		b[i] = rand();
-		c[i] = rand();
-	}
+	////a += b;
+	////a = anvil::VectorCmpEqtiplyAdd(a, b, c);
+	////a = anvil::VectorAnd(a, b);
+	////a = (a & b) | c;
 
-	//a += b;
-	//a = anvil::VectorCmpEqtiplyAdd(a, b, c);
-	//a = anvil::VectorAnd(a, b);
-	//a = (a & b) | c;
+	////enum : uint64_t { mask = 12345 };
+	//int mask = rand() & ((1 << size) - 1);
+	//a = anvil::VectorAnd(b, c, a, mask);
+	////a = anvil::VectorAnd<mask>(b, c, a);
+	////a = anvil::VectorAdd<mask>(b, c, b);
+	////a = anvil::VectorNot(a,a, mask);
 
-	//enum : uint64_t { mask = 12345 };
-	int mask = rand() & ((1 << size) - 1);
-	a = anvil::VectorAnd(b, c, a, mask);
-	//a = anvil::VectorAnd<mask>(b, c, a);
-	//a = anvil::VectorAdd<mask>(b, c, b);
-	//a = anvil::VectorNot(a,a, mask);
+	//std::cout << a[0u] << std::endl;
 
-	std::cout << a[0u] << std::endl;
+	////uint32_t val;
+	////std::cin >> val;
 
-	//uint32_t val;
-	//std::cin >> val;
-
-	//std::bitset<size> a = val;
-	//std::bitset<size> b = anvil::CountOnes(&val, 1);
+	////std::bitset<size> a = val;
+	////std::bitset<size> b = anvil::CountOnes(&val, 1);
 
 
-	//std::cout << a << std::endl;
- //   std::cout << b << std::endl;
-	system("pause");
+	////std::cout << a << std::endl;
+ ////   std::cout << b << std::endl;
+	//system("pause");
 }
