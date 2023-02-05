40bdc16a834ae470d6b317431c428d0e2ae740e3
Adam Smith <adam.george.smith@btinternet.com>
2023-02-05 22:17:19 +0000

    Optimisation
    Base64 decode is nearly twice as fast for the default table.
    May be sightly slower when not using the default table and the data size is small

--- src/anvil/core/Base64.cpp
+++ src/anvil/core/Base64.cpp
@@ -19,15 +19,23 @@ namespace anvil {
 
 	static ANVIL_CONSTEXPR_VAR const char* const g_default_base64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwzyz0123456789+/";
 
-	union Base64Helper {
-		uint32_t u32;
-		uint8_t u8[4u];
-		struct {
-			uint32_t u6_0 : 6;
-			uint32_t u6_1 : 6;
-			uint32_t u6_2 : 6;
-			uint32_t u6_3 : 6;
-		};
+	static const ANVIL_CONSTEXPR_VAR uint8_t g_default_base64_reverse_table[] = { //!< Convert from ASCII to indices in g_default_base64_table
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 62u, 0u, 0u, 0u, 63u,
+		52u, 53u, 54u, 55u, 56u, 57u, 58u, 59u, 60u, 61u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 1u, 2u, 3u, 4u, 5u, 6u, 7u, 8u, 9u, 10u, 11u, 12u, 13u, 14u,
+		15u, 16u, 17u, 18u, 19u, 20u, 21u, 22u, 23u, 24u, 25u, 0u, 0u, 0u, 0u, 0u,
+		0u, 26u, 27u, 28u, 29u, 30u, 31u, 32u, 33u, 34u, 35u, 36u, 37u, 38u, 39u, 40u,
+		41u, 42u, 43u, 44u, 45u, 46u, 47u, 48u, 0u, 50u, 49u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u,
+		0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u
 	};
 
 	// Base64
@@ -37,16 +45,12 @@ namespace anvil {
 	}
 
 	void Base64::Encode(const uint8_t* src, size_t bytes_in, char* dst, size_t& dst_len, const char* const table, char padding) {
-		Base64Helper helper;
-		helper.u32 = 0u;
-
 		dst_len = 0u;
 
 		while (bytes_in >= 3u) {
 			const uint32_t tmp = static_cast<uint32_t>(src[0u]) | (static_cast<uint32_t>(src[1u]) << 8u) | (static_cast<uint32_t>(src[2u]) << 16u);
 			src += 3u;
 			bytes_in -= 3u;
-
 
 			dst[0u] = table[tmp & 63u];
 			dst[1u] = table[(tmp >> 6u) & 63u];
@@ -56,31 +60,29 @@ namespace anvil {
 			dst_len += 4u;
 		}
 
-		helper.u32 = 0u;
 		if (bytes_in >= 2u) {
-			helper.u8[0u] = src[0u];
-			helper.u8[1u] = src[1u];
+			const uint32_t tmp = static_cast<uint32_t>(src[0u]) | (static_cast<uint32_t>(src[1u]) << 8u);
 			src += 2u;
 			bytes_in -= 2u;
 
-			dst[0u] = table[helper.u6_0];
-			dst[1u] = table[helper.u6_1];
-			dst[2u] = table[helper.u6_2];
-			dst[3u] = table[helper.u6_3];
+			dst[0u] = table[tmp & 63u];
+			dst[1u] = table[(tmp >> 6u) & 63u];
+			dst[2u] = table[(tmp >> 12u) & 63];
+			dst[3u] = table[tmp >> 18u];
 			dst[4u] = padding;
 
 			dst += 5u;
 			dst_len += 5u;
 
 		} else if (bytes_in >= 1u) {
-			helper.u8[0u] = src[0u];
+			const uint32_t tmp = static_cast<uint32_t>(src[0u]);
 			src += 1u;
 			bytes_in -= 1u;
 
-			dst[0u] = table[helper.u6_0];
-			dst[1u] = table[helper.u6_1];
-			dst[2u] = table[helper.u6_2];
-			dst[3u] = table[helper.u6_3];
+			dst[0u] = table[tmp & 63u];
+			dst[1u] = table[(tmp >> 6u) & 63u];
+			dst[2u] = table[(tmp >> 12u) & 63];
+			dst[3u] = table[tmp >> 18u];
 			dst[4u] = padding;
 			dst[5u] = padding;
 
@@ -101,19 +103,29 @@ namespace anvil {
 
 		} else if (padded_bytes == 1u) {
 			src_len -= 5u;
-		}
-
+		}	
+		
 		size_t s = (src_len / 4u) * 3u;
 		if (padded_bytes == 2u) s += 1u;
 		else if (padded_bytes == 1u) s += 2u;
-		
+
 		return s;
 	}
 
-	template<bool DEFAULT_TABLE>
-	void Base64Decode(const char* src, size_t src_len, uint8_t* dst, size_t& bytes_out, const char* const table, char padding) {
-		Base64Helper helper;
-		helper.u32 = 0u;
+	void Base64::Decode(const char* src, size_t src_len, uint8_t* dst, size_t& bytes_out, const char* const table, char padding) {
+		const uint8_t* reverse_table;
+		if (table == g_default_base64_table) {
+			reverse_table = g_default_base64_reverse_table;
+
+		} else {
+			uint8_t* t = static_cast<uint8_t*>(_alloca(256u)); 
+			for (size_t c = 0u; c < 256u; ++c) {
+				size_t i = 0;
+				for (uint32_t j = 0; j < 64u; ++j) if (c == table[j]) break; 
+				t[c] = i;
+			}
+			reverse_table = t;
+		}
 
 		bytes_out = 0u;
 
@@ -128,64 +140,46 @@ namespace anvil {
 			src_len -= 5u;
 		}
 
-		const auto DecodeChar = [table](const char c)->uint32_t {
-			if ANVIL_CONSTEXPR_FN(DEFAULT_TABLE) {
-				if (c >= 'A' && c <= 'Z') return c - 'A';
-				if (c >= 'a' && c <= 'z') return (c - 'a') + 26;
-				if (c >= '0' && c <= '9') return (c - '0') + 52;
-				else if (c == '+') return 62;
-				return 63;
-			} else {
-				uint32_t i = 0u;
-				for (i; i < 64; ++i) if (c == table[i]) break;
-				return i;
-			}
-		};
-
 		while (src_len >= 4u) {
 
-			helper.u6_0 = DecodeChar(src[0u]);
-			helper.u6_1 = DecodeChar(src[1u]);
-			helper.u6_2 = DecodeChar(src[2u]);
-			helper.u6_3 = DecodeChar(src[3u]);
+			const uint32_t a = reverse_table[src[0u]];
+			const uint32_t b = reverse_table[src[1u]];
+			const uint32_t c = reverse_table[src[2u]];
+			const uint32_t d = reverse_table[src[3u]];
+
+			uint32_t tmp = a | (b << 6u) | (c << 12u) | (d << 18u);
 			src += 4u;
 			src_len -= 4u;
 
-			dst[0u] = helper.u8[0u];
-			dst[1u] = helper.u8[1u];
-			dst[2u] = helper.u8[2u];
+			dst[0u] = static_cast<uint8_t>(tmp & 255u);
+			dst[1u] = static_cast<uint8_t>((tmp >> 8u) & 255u);
+			dst[2u] = static_cast<uint8_t>(tmp >> 16u);
 			dst += 3u;
 			bytes_out += 3u;
 		}
 
 		if (padded_bytes > 0u) {
 
-			helper.u6_0 = DecodeChar(src[0u]);
-			helper.u6_1 = DecodeChar(src[1u]);
-			helper.u6_2 = DecodeChar(src[2u]);
-			helper.u6_3 = DecodeChar(src[3u]);
+			const uint32_t a = reverse_table[src[0u]];
+			const uint32_t b = reverse_table[src[1u]];
+			const uint32_t c = reverse_table[src[2u]];
+			const uint32_t d = reverse_table[src[3u]];
+
+			uint32_t tmp = a | (b << 6u) | (c << 12u) | (d << 18u);
 			src += 4u;
 			src_len -= 4u;
 
 			if (padded_bytes == 1u) {
-				dst[0u] = helper.u8[0u];
-				dst[1u] = helper.u8[1u];
+				dst[0u] = static_cast<uint8_t>(tmp & 255u);
+				dst[1u] = static_cast<uint8_t>((tmp >> 8u) & 255u);
 				dst += 2u;
 				bytes_out += 2u;
 
 			} else if (padded_bytes == 2u) {
-				dst[0u] = helper.u8[0u];
+				dst[0u] = static_cast<uint8_t>(tmp & 255u);
 				dst += 1u;
 				bytes_out += 1u;
 			}
-		}
-	}
-
-	void Base64::Decode(const char* src, size_t src_len, uint8_t* dst, size_t& bytes_out, const char* const table, char padding) {
-		if (table == g_default_base64_table) {
-			Base64Decode<true>(src, src_len, dst, bytes_out, table, padding);
-		} else {
-			Base64Decode<false>(src, src_len, dst, bytes_out, table, padding);
 		}
 	}
 
