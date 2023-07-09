1ebe1bf808fb65a6459895b3dc4c8cb245b9215e
Adam Smith <adam.george.smith@btinternet.com>
2023-07-09 21:41:42 +0100

    Unit Test Cleanup

--- VisualStudio/Anvil/UnitTests/UnitTests.cpp
+++ VisualStudio/Anvil/UnitTests/UnitTests.cpp
@@ -9,6 +9,38 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace UnitTests
 {
 
+	void TestRange64Bitwise(const std::function<void(uint64_t)>& fn) {
+		// Testing every 64-bit integer takes too long so approximate a range of different values
+
+		// Zero
+		fn(0);
+
+		// All indidual bits
+		{
+			uint64_t bit = 1u;
+			for (int i = 0; i < 64; ++i) {
+				fn(i);
+				bit <<= 1ull;
+			}
+		}
+
+		// Different combinations of every 16-bit integer
+		for (uint64_t i = 0u; i < UINT16_MAX; ++i) {
+			uint64_t val = i;
+			fn(val);
+
+			fn(val << 8ull);
+			fn(val << 16ull);
+			fn(val << 24ull);
+			fn(val << 32ull);
+
+			val |= val << 16ull;
+			fn(val);
+			val |= val << 32ull;
+			fn(val);
+		}
+	}
+
 	TEST_CLASS(LeadingZeroCount)
 	{
 	public:
@@ -26,90 +58,12 @@ namespace UnitTests
 
 			return result;
 		}
-		
-		TEST_METHOD(DoesCPPHandleZero)
+
+		TEST_METHOD(IsBSRImplementationSameAsCpp)
 		{
-			uint64_t val = 0u;
+			if (!ANVIL_HW_LZCNTA) Assert::Fail(L"CPU does not support BSR instruction");
 
-			size_t result = anvil::detail::lzcount8_c((uint8_t) val);
-			Assert::AreEqual(result, (size_t) 8);
-
-			result = anvil::detail::lzcount16_c((uint16_t)val);
-			Assert::AreEqual(result, (size_t)16);
-
-			result = anvil::detail::lzcount32_c((uint32_t)val);
-			Assert::AreEqual(result, (size_t)32);
-
-			result = anvil::detail::lzcount64_c((uint64_t)val);
-			Assert::AreEqual(result, (size_t)64);
-		}
-
-		void TestRange(const std::function<void(uint64_t)>& fn, uint64_t min, uint64_t max) {
-			uint64_t range = max - min;
-			if (range < UINT16_MAX) {
-				for (uint64_t i = min; i <= max; ++i) fn(i);
-			} else {
-				for (uint64_t i = 0u; i < UINT16_MAX; ++i) {
-					uint64_t val = i + min;
-					fn(val);
-					val <<= 4;
-					if(val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-				}
-			}
-		}
-
-		void TestRange(const std::function<void(uint64_t)>& fn) {
-			TestRange(fn, 0u, UINT64_MAX);
-		}
-
-		TEST_METHOD(DoesCPPHandleAnyIndividualBit)
-		{
-			uint64_t val = 1u;
-			size_t correct = 0u;
-			size_t result;
-
-			for (size_t i = 0u; i < 63; ++i) {
-				if (val <= UINT8_MAX) {
-					result = anvil::detail::lzcount8_c((uint8_t)val);
-					Assert::AreEqual(result, correct);
-				}
-
-				if (val <= UINT16_MAX) {
-					result = anvil::detail::lzcount16_c((uint16_t)val);
-					Assert::AreEqual(result, correct);
-				}
-
-				if (val <= UINT32_MAX) {
-					result = anvil::detail::lzcount32_c((uint32_t)val);
-					Assert::AreEqual(result, correct);
-				}
-
-				result = anvil::detail::lzcount64_c((uint64_t)val);
-				Assert::AreEqual(result, correct);
-
-				val <<= 1ull;
-				++correct;
-			}
-		}
-		TEST_METHOD(DoesHWAccelerationAHaveSameResultAsCpp)
-		{
-			if (!ANVIL_HW_LZCNTA) Assert::Fail(L"CPU does not support HW LZCNT (A Implementation)");
-
-			TestRange([this](uint64_t i)->void {
+			TestRange64Bitwise([this](uint64_t i)->void {
 				uint64_t c;
 				uint64_t hw;
 
@@ -137,11 +91,11 @@ namespace UnitTests
 			});
 		}
 
-		TEST_METHOD(DoesHWAccelerationBHaveSameResultAsCpp)
+		TEST_METHOD(IsLZCNTImplementationSameAsCpp)
 		{
-			if (!ANVIL_HW_LZCNTB) Assert::Fail(L"CPU does not support HW LZCNT (B Implementation)");
+			if (!ANVIL_HW_LZCNTB) Assert::Fail(L"CPU does not support LZCNT instruction");
 
-			TestRange([this](uint64_t i)->void {
+			TestRange64Bitwise([this](uint64_t i)->void {
 				uint64_t c;
 				uint64_t hw;
 
@@ -169,13 +123,13 @@ namespace UnitTests
 			});
 		}
 
-		TEST_METHOD(DoesCppHaveSameResultAsTestImplementation)
+		TEST_METHOD(IsCPPImplementationSameAsSlowTest)
 		{
-			TestRange([this](uint64_t i)->void {
+			TestRange64Bitwise([this](uint64_t i)->void {
 
 				if (i <= UINT32_MAX) {
 					uint32_t c, test;
-					c = anvil::detail::lzcount32_c((uint32_t)i);
+					c = anvil::detail::lzcount64_c((uint32_t)i);
 					test = TestLZ((uint32_t)i);
 					Assert::AreEqual(c, test);
 				}
@@ -187,38 +141,6 @@ namespace UnitTests
 	TEST_CLASS(TrailingZeroCount)
 	{
 	public:
-
-		void TestRange(const std::function<void(uint64_t)>& fn, uint64_t min, uint64_t max) {
-			uint64_t range = max - min;
-			if (range < UINT16_MAX) {
-				for (uint64_t i = min; i <= max; ++i) fn(i);
-			} else {
-				for (uint64_t i = 0u; i < UINT16_MAX; ++i) {
-					uint64_t val = i + min;
-					fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-					val <<= 4;
-					if (val < max) fn(val);
-				}
-			}
-		}
-
-		void TestRange(const std::function<void(uint64_t)>& fn) {
-			TestRange(fn, 0u, UINT64_MAX - 1ull);
-		}
 
 
 		size_t TestTZ(uint64_t val) {
@@ -239,87 +161,11 @@ namespace UnitTests
 
 			return result;
 		}
-		TEST_METHOD(DoesCppHandleZero)
+		TEST_METHOD(IsBSRImplementationSameAsCpp)
 		{
-			uint64_t val = 0u;
+			if (!ANVIL_HW_TZCNTA) Assert::Fail(L"CPU does not support BSR instruction");
 
-			size_t result = anvil::detail::lzcount8_c((uint8_t) val);
-			Assert::AreEqual(result, (size_t) 8);
-
-			result = anvil::detail::lzcount16_c((uint16_t)val);
-			Assert::AreEqual(result, (size_t)16);
-
-			result = anvil::detail::lzcount32_c((uint32_t)val);
-			Assert::AreEqual(result, (size_t)32);
-
-			result = anvil::detail::lzcount64_c((uint64_t)val);
-			Assert::AreEqual(result, (size_t)64);
-		}
-
-		TEST_METHOD(DoesCppBitHandleAnyIndividualBit_8)
-		{
-			uint64_t val = 1u;
-			size_t correct = 7u;
-			size_t result;
-
-			for (size_t i = 0u; i < 8; ++i) {
-				result = anvil::detail::tzcount8_c((uint8_t)val);
-				Assert::AreEqual(result, correct);
-
-				val <<= 1ull;
-				--correct;
-			}
-		}
-		TEST_METHOD(DoesCppBitHandleAnyIndividualBit_16)
-		{
-			uint64_t val = 1u;
-			size_t correct = 15u;
-			size_t result;
-
-			for (size_t i = 0u; i < 16; ++i) {
-				result = anvil::detail::tzcount16_c((uint16_t)val);
-				Assert::AreEqual(result, correct);
-
-				val <<= 1ull;
-				--correct;
-			}
-		}
-
-		TEST_METHOD(DoesCppBitHandleAnyIndividualBit_32)
-		{
-			uint64_t val = 1u;
-			size_t correct = 31u;
-			size_t result;
-
-			for (size_t i = 0u; i < 32; ++i) {
-				result = anvil::detail::tzcount32_c((uint32_t)val);
-				Assert::AreEqual(result, correct);
-
-				val <<= 1ull;
-				--correct;
-			}
-		}
-
-		TEST_METHOD(DoesCppBitHandleAnyIndividualBit_64)
-		{
-			uint64_t val = 1u;
-			size_t correct = 63u;
-			size_t result;
-
-			for (size_t i = 0u; i < 64; ++i) {
-				result = anvil::detail::tzcount64_c((uint64_t)val);
-				Assert::AreEqual(result, correct);
-
-				val <<= 1ull;
-				--correct;
-			}
-		}
-
-		TEST_METHOD(DoesHWAccelerationAHaveSameResultAsCpp)
-		{
-			if (!ANVIL_HW_TZCNTA) Assert::Fail(L"CPU does not support HW LZCNT (A implementation)");
-
-			TestRange([this](uint64_t i)->void {
+			TestRange64Bitwise([this](uint64_t i)->void {
 				uint64_t c;
 				uint64_t hw;
 
@@ -347,11 +193,11 @@ namespace UnitTests
 			});
 		}
 
-		TEST_METHOD(DoesHWAccelerationBHaveSameResultAsCpp)
+		TEST_METHOD(IsTZCNTImplementationSameAsCpp)
 		{
-			if (!ANVIL_HW_TZCNTB) Assert::Fail(L"CPU does not support HW LZCNT (B implementation)");
+			if (!ANVIL_HW_TZCNTB) Assert::Fail(L"CPU does not support TZCNT instruction");
 
-			TestRange([this](uint64_t i)->void {
+			TestRange64Bitwise([this](uint64_t i)->void {
 				uint64_t c;
 				uint64_t hw;
 
@@ -379,13 +225,13 @@ namespace UnitTests
 			});
 		}
 
-		TEST_METHOD(DoesCppHaveSameResultAsTestImplementation)
+		TEST_METHOD(IsCPPImplementationSameAsSlowTest)
 		{
-			TestRange([this](uint64_t i)->void {
+			TestRange64Bitwise([this](uint64_t i)->void {
 
 				if (i <= UINT32_MAX) {
 					uint32_t c, test;
-					c = anvil::detail::tzcount32_c((uint32_t)i);
+					c = anvil::detail::tzcount64_c((uint32_t)i);
 					test = TestTZ((uint32_t)i);
 					Assert::AreEqual(c, test);
 				}
