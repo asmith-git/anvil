919359896f503e2ac776f6789e6516f4063ecf41
Adam Smith <adam.george.smith@btinternet.com>
2023-11-04 13:01:22 +0000

    LocalPointer move assignment

--- VisualStudio/Anvil/UnitTests/TestLocalPointer.cpp
+++ VisualStudio/Anvil/UnitTests/TestLocalPointer.cpp
@@ -33,8 +33,7 @@ namespace anvil { namespace core {
 		{
 			int counter = 0;
 
-			class DestructorTest
-			{
+			class DestructorTest {
 			public:
 				int& counter;
 
@@ -68,6 +67,49 @@ namespace anvil { namespace core {
 			}
 			Assert::AreEqual(1000, counter, L"Destructor was not called the expected number of times (for loop)");
 		}
+
+
+		TEST_METHOD(VirtualDestruction)
+		{
+			int base_counter = 0;
+			int child_counter = 0;
+
+			class IDestructorTest {
+			private:
+				int& _base_counter;
+			public:
+				IDestructorTest(int& counter) :
+					_base_counter(counter)
+				{}
+
+				virtual ~IDestructorTest() {
+					++_base_counter;
+				}
+			};
+
+			class DestructorTest final : IDestructorTest {
+			private:
+				int& _child_counter;
+			public:
+				DestructorTest(int& base_counter, int& child_counter) :
+					IDestructorTest(base_counter),
+					_child_counter(child_counter)
+				{}
+
+				virtual ~DestructorTest() {
+					++_child_counter;
+				}
+			};
+
+			base_counter = 0;
+			child_counter = 0;
+			for(int i = 0; i < 1000; ++i) {
+				anvil::LocalPointer<IDestructorTest, sizeof(DestructorTest)> ptr;
+				ptr.reset<DestructorTest>(std::ref<int>(base_counter), std::ref<int>(child_counter));
+			}
+			Assert::AreEqual(1000, base_counter, L"Base class destructor was not called the expected number of times");
+			Assert::AreEqual(1000, child_counter, L"Child class destructor was not called the expected number of times");
+		}
 		TEST_METHOD(ReleaseMovable)
 		{
 			{
@@ -77,6 +119,7 @@ namespace anvil { namespace core {
 				Assert::IsTrue(str == "test", L"String not allocated correctly");
 			}
 		}
+
 		TEST_METHOD(ReleaseUnmovable)
 		{
 			class Unmovable {
@@ -92,6 +135,20 @@ namespace anvil { namespace core {
 				anvil::LocalPointer<Unmovable> tmp;
 				tmp.reset<Unmovable>();
 				tmp.release();
+			}
+		}
+
+		TEST_METHOD(Move)
+		{
+			{
+				anvil::LocalPointer<std::string> a("a");
+				anvil::LocalPointer<std::string> b("b");
+				Assert::IsTrue(*a == "a", L"A string not allocated correctly");
+				Assert::IsTrue(*b == "b", L"B string not allocated correctly");
+
+				a = std::move(b);
+				Assert::IsTrue(*a == "b", L"B string not moved correctly");
+				Assert::IsTrue(b->empty(), L"Expected B string to be empty");
 			}
 		}
 	};
--- include/anvil/core/LocalPointer.hpp
+++ include/anvil/core/LocalPointer.hpp
@@ -27,8 +27,153 @@ namespace anvil {
 	*	\tparam T The type to allocate.
 	*	\tparam BYTES The number of bytes to reserve to allocate. Increase from the default value if sub-classes of T are required.
 	*/
-	template<class T, size_t BYTES = sizeof(T)>
-	class LocalPointer {
+	template<class T, size_t BYTES = sizeof(T), bool MOVABLE = std::is_move_assignable<T>::value && std::is_default_constructible<T>::value>
+	class LocalPointer;
+
+	template<class T, size_t BYTES>
+	class LocalPointer<T, BYTES, true> {
+	private:
+		uint8_t _local_memory[BYTES];	//!< Local memory for allocations
+		bool _is_allocated;				//!< True if something is currently allocated in _local_memory
+	public:
+		typedef T element_type;
+		typedef element_type* pointer;
+		typedef const element_type* const_pointer;
+
+		typedef LocalPointer<element_type, BYTES> this_t;
+
+		LocalPointer(this_t&&) = delete;
+		LocalPointer(const this_t&) = delete;
+
+		LocalPointer() :
+			_local_memory{},
+			_is_allocated(false)
+		{}
+
+		~LocalPointer() {
+			Deallocate();
+		}
+
+		template<class ...PARAMS>
+		LocalPointer(PARAMS... params) :
+			LocalPointer()
+		{
+			reset<element_type>(params...);
+		}
+
+		template<class T2 = element_type>
+		this_t& operator=(const this_t& other) {
+			if (other._is_allocated) {
+				if (_is_allocated) {
+					*get() = *other;
+				} else {
+					reset<T2>(*other);
+				}
+			} else {
+				Deallocate();
+			}
+			return *this;
+		}
+
+		template<class T2 = element_type>
+		this_t& operator=(this_t&& other) {
+			if (other._is_allocated) {
+				if (_is_allocated) {
+					*get() = std::move(*other);
+				} else {
+					reset<T2>(std::move(*other));
+				}
+			} else {
+				Deallocate();
+			}
+			return *this;
+		}
+
+		void Deallocate() {
+			if (_is_allocated) {
+				reinterpret_cast<element_type*>(_local_memory)->~T();
+				_is_allocated = false;
+			}
+		}
+
+		template<class NEW_T = element_type>
+		inline std::enable_if<std::is_default_constructible<NEW_T>::value, void>::type reset() {
+			// Compile-time safety checks
+			static_assert(std::is_same<element_type, NEW_T>::value || std::is_base_of<element_type, NEW_T>::value, "anvil::LocalPointer::Allocate : class is not allowed");
+			static_assert(sizeof(NEW_T) <= BYTES, "anvil::LocalPointer::Allocate : Not enough memory to allocate class");
+
+			Deallocate();
+			new(_local_memory) NEW_T();
+			_is_allocated = true;
+		}
+
+		template<class NEW_T, class ...PARAMS>
+		inline void reset(PARAMS... params) {
+			// Compile-time safety checks
+			static_assert(std::is_same<element_type, NEW_T>::value || std::is_base_of<element_type, NEW_T>::value, "anvil::LocalPointer::Allocate : class is not allowed");
+			static_assert(sizeof(NEW_T) <= BYTES, "anvil::LocalPointer::Allocate : Not enough memory to allocate class");
+
+			Deallocate();
+			new(_local_memory) NEW_T(params...);
+			_is_allocated = true;
+		}
+
+		inline element_type* get() {
+			return _is_allocated ? reinterpret_cast<element_type*>(_local_memory) : nullptr;
+		}
+
+		inline const element_type* get() const {
+			return _is_allocated ? reinterpret_cast<const element_type*>(_local_memory) : nullptr;
+		}
+
+		inline element_type release() {
+			element_type tmp = std::move(*get());
+			Deallocate();
+			return tmp;
+		}
+
+		inline operator bool() const {
+			return _is_allocated;
+		}
+
+		inline element_type* operator->() {
+			return get();
+		}
+
+		inline const element_type* operator->() const {
+			return get();
+		}
+
+		inline element_type& operator*() {
+			ANVIL_DEBUG_ASSERT(_is_allocated, "anvil::LocalPointer::get : Pointer is not allocated");
+			return *get();
+		}
+
+		inline const element_type& operator*() const {
+			ANVIL_DEBUG_ASSERT(_is_allocated, "anvil::LocalPointer::get : Pointer is not allocated");
+			return *get();
+		}
+
+		inline bool operator!=(const void* other) const {
+			return other == nullptr ? _is_allocated : this != other;
+		}
+
+		inline bool operator==(const void* other) const {
+			return !operator!=(other);
+		}
+
+		inline bool operator==(const this_t& other) const {
+			return _is_allocated ? this == &other : !other._is_allocated;
+		}
+
+		inline bool operator!=(const this_t& other) const {
+			return !operator==(other);
+		}
+	};
+
+
+	template<class T, size_t BYTES>
+	class LocalPointer<T, BYTES, false> {
 	private:
 		uint8_t _local_memory[BYTES]; //!< Local memory for allocations
 		bool _is_allocated;				//!< True if something is currently allocated in _local_memory
@@ -41,8 +186,8 @@ namespace anvil {
 
 		LocalPointer(this_t&&) = delete;
 		LocalPointer(const this_t&) = delete;
-		LocalPointer& operator=(this_t&&) = delete;
-		LocalPointer& operator=(const this_t&) = delete;
+		this_t& operator=(this_t&&) = delete;
+		this_t& operator=(const this_t&) = delete;
 
 		LocalPointer() :
 			_local_memory{},
@@ -98,14 +243,7 @@ namespace anvil {
 		}
 
 		template<class T2 = element_type>
-		inline std::enable_if<std::is_move_assignable<T2>::value && ! std::is_abstract<T2>::value, element_type>::type release() {
-			element_type tmp = std::move(*get());
-			Deallocate();
-			return tmp;
-		}
-
-		template<class T2 = element_type>
-		inline std::enable_if<std::is_abstract<T2>::value || !std::is_move_assignable<T2>::value, void>::type release() {
+		inline void release() {
 			Deallocate();
 		}
 
